63a6038da69e3013dd418bfaab30c7e004288e63
meow <you@example.com>
2019-12-01 15:26:47 +0900

    change data structure from JSON to XML

--- animeface_result2xml.py
+++ animeface_result2xml.py
@@ -15,6 +15,8 @@ import copy
 from pprint import pprint as print
 import dicttoxml
 from bs4 import BeautifulSoup
+import xml.etree.ElementTree as ET
+
 
 from PIL import Image
 
@@ -44,6 +46,7 @@ def image_validator(im_path, valid_img=['png', 'jpeg', 'gif']):
     else:
         return False
 
+
 def _insert_bbox(src_d, tgt_d):
     tgt_d['xmin'] = src_d['x']
     tgt_d['ymin'] = src_d['y']
@@ -51,7 +54,17 @@ def _insert_bbox(src_d, tgt_d):
     tgt_d['ymax'] = str(int(src_d['y']) + int(src_d['height']))
 
 
-def insert_det_result_to_template(det_result_list, template, im_size):
+def mk_bbox(src_d):
+    d = {}
+    d['xmin'] = str(src_d['x'])
+    d['ymin'] = str(src_d['y'])
+    d['xmax'] = str(int(src_d['x']) + int(src_d['width']))
+    d['ymax'] = str(int(src_d['y']) + int(src_d['height']))
+
+    return d
+
+
+def _insert_det_result_to_template(det_result_list, template, im_size):
 
     output_dic_list = []
 
@@ -120,9 +133,7 @@ def output_xml(updated_result_dict):
     output_result = dom.toprettyxml()
 
     return output_result
-
     
-
 
 def dirty_postedit1(dic_):
     if dic_['annotation']['object'] == []:
@@ -142,7 +153,108 @@ def dirty_postedit2(output_result):
     return output_result
 
 
+def _add_elem(name, bbox_dic, et):
+    o = ET.SubElement(et,  "object")
+    e = ET.SubElement(o,  "name")
+    e.text = name
+
+    e = ET.SubElement(o,  "pose")
+    e.text = 'Unspecified'
+
+    e = ET.SubElement(o,  "truncated")
+    e.text = '1'
+
+    e = ET.SubElement(o,  "difficult")
+    e.text = '0'
+
+    e = ET.SubElement(o,  "bndbox")
+    b = ET.SubElement(e,  "xmin")
+    b.text = bbox_dic['xmin']
+
+    b = ET.SubElement(e,  "ymin")
+    b.text = bbox_dic['ymin']
+
+    b = ET.SubElement(e,  "xmax")
+    b.text = bbox_dic['xmax']
+
+    b = ET.SubElement(e,  "ymax")
+    b.text = bbox_dic['ymax']
+
+
+def insert_det_result_to_template(det_result_list, tgt_xml, im_size):
+
+    output_dic_list = []
+
+    (w, h) = im_size
+
+    # 小さい物体の辺のサイズ
+    small_obj_edge = max((min(w, h) / 20), 10)
+
+    tgt_xml[4][0].text = str(w)
+    tgt_xml[4][1].text = str(h)
+
+    for det_result in det_result_list:
+        print(det_result)
+        d = mk_bbox(det_result["face"])
+        _add_elem("face", d, tgt_xml)
+        
+        d = mk_bbox(det_result["eyes"]["right"])
+        _add_elem("right_eye", d, tgt_xml)
+        
+        d = mk_bbox(det_result["eyes"]["left"])
+        _add_elem("left_eye", d, tgt_xml)
+        
+        det_result["nose"]["width"] = small_obj_edge
+        det_result["nose"]["height"] = small_obj_edge
+        d = mk_bbox(det_result["nose"])
+        _add_elem("nose", d, tgt_xml)
+        
+        d = mk_bbox(det_result["mouth"])
+        _add_elem("mouth", d, tgt_xml)
+        
+        det_result["chin"]["width"] = small_obj_edge
+        det_result["chin"]["height"] = small_obj_edge
+        d = mk_bbox(det_result["chin"])
+        _add_elem("chin", d, tgt_xml)
+
+
+    return tgt_xml
+
+
 def convert_bbox2xml(image_dir, xml_output_dir, output_filelist_path):
+    
+    # load xml template
+    template_path = this_file_dir / 'data/template.xml'
+
+    image_dir = Path(image_dir)
+    assert image_dir.exists(), image_dir
+
+    # import pdb; pdb.set_trace()
+
+    for file_path in image_dir.glob('**/*'):
+        print(file_path)
+        if file_path.is_dir():
+            continue
+        if not image_validator(file_path):
+            continue
+
+        tree = ET.parse(str(template_path))
+        element_root = tree.getroot()
+
+        im = Image.open(str(file_path))
+        w, h = im.size
+
+        est_result = detect_animeface(file_path)
+        tgt_xml = insert_det_result_to_template(est_result, element_root, (w, h))
+        # import pdb; pdb.set_trace()
+
+        output_file_name = generate_output_file_name(file_path, xml_output_dir)
+
+        tree.write(str(output_file_name), encoding='utf-8', xml_declaration=False)
+
+
+
+def _convert_bbox2xml(image_dir, xml_output_dir, output_filelist_path):
     
     # load xml template
     template_path = this_file_dir / 'data/template.xml'
@@ -179,6 +291,7 @@ def convert_bbox2xml(image_dir, xml_output_dir, output_filelist_path):
             f.write(output_result)
 
         print(updated_result)
+
 
 if __name__ == '__main__':
     argvs = sys.argv
