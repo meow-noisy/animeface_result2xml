fb2323227c35df2f0ecc3f057e73a12fdee5ff50
meow <meow@example.com>
2019-12-18 21:01:57 +0900

    検出結果の描画関数の追加。また、ファイルからの呼び出しで描画した画像を出力するようにした。

--- animeface_poor_caller.py
+++ animeface_poor_caller.py
@@ -1,16 +1,29 @@
-# a poor python wrapper of animeface-2009
+# a poor python caller of animeface-2009
 # call rb module via shell by using subprocess...
 
 import subprocess
 
 import sys
 import json
-
+from PIL import Image
 from pathlib import Path
 
 this_file_dir = (Path(__file__).resolve()).parent
 
 import re
+
+
+def _draw_det_image(im, result_list, output_path):
+    draw = ImageDraw.Draw(im)
+    for res in result_list:
+        draw.rectangle((res['face']['x'], res['face']['y'], res['face']['x'] + res['face']['width'], res['face']['y'] + res['face']['height']), outline=(0, 255, 0))
+        draw.rectangle((res["eyes"]["right"]['x'], res["eyes"]["right"]['y'], res["eyes"]["right"]['x'] + res["eyes"]["right"]['width'], res["eyes"]["right"]['y'] + res["eyes"]["right"]['height']), outline=(0, 255, 0))
+        draw.rectangle((res["eyes"]["left"]['x'], res["eyes"]["left"]['y'], res["eyes"]["left"]['x'] + res["eyes"]["left"]['width'], res["eyes"]["left"]['y'] + res["eyes"]["left"]['height']), outline=(0, 255, 0))
+        draw.rectangle((res["mouth"]['x'], res["mouth"]['y'], res["mouth"]['x'] + res["mouth"]['width'], res["mouth"]['y'] + res["mouth"]['height']), outline=(0, 255, 0))
+        draw.rectangle((res["nose"]['x'], res["nose"]['y'], res["nose"]['x'] + 1, res["nose"]['y'] + 1), outline=(0, 255, 0))
+        draw.rectangle((res["chin"]['x'], res["chin"]['y'], res["chin"]['x'] + 1, res["chin"]['y'] + 1), outline=(0, 255, 0))
+
+    im.save(output_path)
 
 
 def detect_animeface(im_path):
@@ -28,15 +41,26 @@ def detect_animeface(im_path):
     #ret = ret.replace("\n1 faces\nSee sample_out.png\n", '')
     #ret = re.sub(r"\n.+ faces\nSee .+\n", '', ret)
     # print(im_path, ret)
-    dic = json.loads(ret)
+    list_ = json.loads(ret)
 
-    return dic
+    return list_
 
 
 if __name__ == '__main__':
+
+    from PIL import Image, ImageDraw
+
     argvs = sys.argv
-    im_path = argvs[1]
+    im_path = Path(argvs[1])
 
-    d = detect_animeface(im_path)
+    assert im_path.exists(), im_path
+    
+    result_list = detect_animeface(im_path)
 
-    print(d)
+    im = Image.open(str(im_path))
+    # output_path = im_path.parent / (im_path.stem + '_out.png')
+    output_path = im_path.stem + '_out.png'
+    _draw_det_image(im, result_list, output_path)
+
+
+    print(result_list)
